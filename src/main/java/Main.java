78c4cb560c9b5402f46284b5bd0d85e990acd647
יחיאל <yehieldimenshtein@gmail.com>
2024-05-21 21:57:03 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Window window=new Window();
         window.showWindow();
-//        char[] chars = ("Yechiel"+"\n"+" its my name.").toCharArray();
-//        List<Integer> list = new ArrayList<>();
-//        for (char c:chars){
-//            list.add((int) c);
-//        }
-//        for (Integer integer:list){
-//            System.out.print((char) integer.intValue());
-//        }
+
 
     }
 }
